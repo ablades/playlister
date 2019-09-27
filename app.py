21c74a06de7d80b9be972bdecfe17f011876db7d
ablades <audaris@gmail.com>
2019-09-27 13:19:22 -0700

    allow playlist creation

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 client = MongoClient()
-db = client.Playliter
+db = client.Playlister
 playlists = db.playlists
 
 app = Flask(__name__)
@@ -16,6 +16,25 @@ def playlists_index():
     #]
     return render_template('playlists_index.html', playlists=playlists.find())
 
+#Gets the post request from the form routed to /playlists
+#Redirects
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    """Submit a new playlist."""
+    #Creates new playlist dictionary object from form
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos' : request.form.get('videos').split()
+    }
+    #inserts into playlists db
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
+
+@app.route('/playlists/new')
+def playlists_new():
+    """Create a new playlist."""
+    return render_template('playlists_new.html')
 if __name__ == "__main__":
     app.run(debug=True)
     
