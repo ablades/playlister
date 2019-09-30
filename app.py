ecff8a2d9492b051bd2dc09fcff8ad3e079b90f0
ablades <audaris@gmail.com>
2019-09-30 11:42:58 -0700

    allow users to see a single playlist

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
+#Creates Mongo Client that connects to default host
 client = MongoClient()
+#Gets the database from client called Playlister (creates it if it doesnt exist)
 db = client.Playlister
+#Retrieves the collection(group of documents)
 playlists = db.playlists
 
 app = Flask(__name__)
@@ -10,10 +14,6 @@ app = Flask(__name__)
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
-    #playlists = [
-    #{ 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    #{ 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-    #]
     return render_template('playlists_index.html', playlists=playlists.find())
 
 #Gets the post request from the form routed to /playlists
@@ -25,16 +25,27 @@ def playlists_submit():
     playlist = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
-        'videos' : request.form.get('videos').split()
+        'videos' : request.form.get('videos').split(),
+        'rating' : request.form.get('rating')
     }
-    #inserts into playlists db
-    playlists.insert_one(playlist)
-    return redirect(url_for('playlists_index'))
+    #inserts into playlists and stores the new id in playlist_id
+    playlist_id = playlists.insert_one(playlist).inserted_id
+    #returns url given by playlistshow function with playlist_id parameter
+    return redirect(url_for('playlists_show',playlist_id=playlist_id))
 
 @app.route('/playlists/new')
 def playlists_new():
     """Create a new playlist."""
     return render_template('playlists_new.html')
+
+#Route to single playlist. Playlist_id is added to route
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({{'id': ObjectId(playlist_id)}})
+    return render_template('playlists_show.html',playlist=playlist)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
     
